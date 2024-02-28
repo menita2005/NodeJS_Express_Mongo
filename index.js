faab76b0d7f0ad4c71fc4d070c9831a6c8bccf97
Sebastian <menaloaizasebastian@gmail.com>
2024-02-28 14:55:49 -0500

    ayuda

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Conexion a la base de datos MongoDB
+mongoose.connect('mongodb://localhost:27017/userscoursesdb')
+    .then(() => console.log('Conectado a MongoDB...'))
+    .catch(err => console.error('No se pudo conectar con MongoDB:', err));
+
+// Middleware 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log('API REST Ok, y ejecutándose...');
+});
