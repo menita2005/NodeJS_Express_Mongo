1083fea9c26eb55eeb2f50a2dbef96b3bf7aaddf
Sebastian <menaloaizasebastian@gmail.com>
2024-03-14 03:08:53 -0500

    añadiendo controladores de tipo put y delete para curso

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -16,7 +16,47 @@ async function crearCurso(body) {
     });
     return await curso.save();
 }
+// Función asincrónica para editar un curso
+async function actualizarCurso(id, body) {
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set: {
+            titulo: body.titulo,
+            descripcion: body.descripcion
+        }
+    }, { new: true });
+    return curso;
+}
 
+// Endpoint de tipo put para cursos
+ruta.put('/:id', (req, res) => {
+    let resultado = actualizarCurso(req.params.id, req.body);
+    resultado.then(curso => {
+        res.json(
+            curso
+        );
+    }).catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+//funcion asincronica para inactivar un curso
+async function desactivarCurso(id){
+    let curso = await Curso.findByIdAndUpdate(id,{
+        $set:{
+            estado:false
+        }
+    },{new:true});
+    return curso;
+}
+//endpoint de tipo delete para curso
+ruta.delete('/:id',(req,res)=>{
+    let resultado = desactivarCurso(req.params.id);
+    resultado.then(curso => {
+        res.json(curso);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
 // Endpoint de tipo POST para crear un curso
 ruta.post('/', (req, res) => {
     console.log('Solicitud POST recibida:', req.body); // Agregar mensaje de depuración
